b152b7ca42b55c38869b54f1e77ed3fce00efd2d
João Victor <78668449+Kilezico@users.noreply.github.com>
2025-06-11 22:16:26 -0300

    Update lis.cpp

--- lib/DP/lis.cpp
+++ lib/DP/lis.cpp
@@ -1,11 +1,11 @@
 int LIS()
 {
-    vector<int> aux = {0};
-    for (int i=1; i<n; i++) {
-        if (seq[i] > aux.back()) {
+    vector<int> aux;
+    for (int i=0; i<(int) seq.size(); i++) {
+        auto it = upper_bound(aux.begin(), aux.end(), seq[i]);
+        if (it == aux.end()) {
             aux.push_back(seq[i]);
         } else {
-            auto it = lower_bound(aux.begin(), aux.end(), seq[i]);
             *it = seq[i];
         }
     }
